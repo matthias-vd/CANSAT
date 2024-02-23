2732e32e723a43d815216353d84584b05741b6d1
VLOT-WESSA <sam.wesemael@vlot.be>
2024-02-23 13:54:13 +0100

    Initial Commit

---
+++ Code_CircuitPython/bnotest.py
@@ -0,0 +1,37 @@
+import time
+import board
+import adafruit_bno055
+import busio
+
+i2c = busio.I2C(board.GP9,board.GP8)  # uses board.SCL and board.SDA
+# i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
+sensor = adafruit_bno055.BNO055_I2C(i2c)
+
+
+
+last_val = 0xFFFF
+
+
+def temperature():
+    global last_val  # pylint: disable=global-statement
+    result = sensor.temperature
+    if abs(result - last_val) == 128:
+        result = sensor.temperature
+        if abs(result - last_val) == 128:
+            return 0b00111111 & result
+    last_val = result
+    return result
+
+
+while True:
+    print("Temperature: {} degrees C".format(sensor.temperature))
+    print("Accelerometer (m/s^2): {}".format(sensor.acceleration))
+    print("Magnetometer (microteslas): {}".format(sensor.magnetic))
+    print("Gyroscope (rad/sec): {}".format(sensor.gyro))
+    print("Euler angle: {}".format(sensor.euler))
+    print("Quaternion: {}".format(sensor.quaternion))
+    print("Linear acceleration (m/s^2): {}".format(sensor.linear_acceleration))
+    print("Gravity (m/s^2): {}".format(sensor.gravity))
+    print()
+
+    time.sleep(3)
