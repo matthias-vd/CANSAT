3c4a6b96aa3de360f781d1799cb7a26e5ab66d9b
VLOT-WESSA <sam.wesemael@vlot.be>
2024-02-23 16:22:43 +0100

    commentaar BMP

--- Code_CircuitPython/bmp280circuit.py
+++ Code_CircuitPython/bmp280circuit.py
@@ -4,15 +4,12 @@ import time
 import adafruit_bmp280
 
 # Create I2C bus
-i2c = busio.I2C(board.GP9, board.GP8)
+i2c = busio.I2C(board.GP9, board.GP8)               # Dit werkt voor connectie met Qwicc, pas pins aan indien nodig.
 
 # Create BMP280 object
 bmp280 = adafruit_bmp280.Adafruit_BMP280_I2C(i2c)
 
-# Optionally, set the BMP280's sea level pressure in hPa for more accurate altitude measurements
-bmp280.sea_level_pressure = 1013.25
-
 while True:
-    print("Temperature: %0.1f C" % bmp280.temperature)
-    print("Pressure: %0.1f hPa" % bmp280.pressure)
+    print("Temperature: %0.1f C" % bmp280.temperature)     # Geef temperatuur in °C
+    print("Pressure: %0.1f hPa" % bmp280.pressure)         # Geef druk in hPa
     time.sleep(1)
--- README.md
+++ README.md
@@ -6,4 +6,5 @@ Dit bevat testcode voor Raspberry Pi Pico
 
 Getest voor MicroPython 1.18 en CircuitPython 8.0.4 (Zie 'Besturingssystemen')
 
+**Installeren van CircuitPython:** 
 Begin met uw Pico losgekoppeld van USB. Houd de BOOTSEL-knop ingedrukt en sluit de Pico aan op USB terwijl u de knop ingedrukt houdt (niet loslaten!). Blijf de BOOTSEL-knop ingedrukt houden totdat de RPI-RP2 drive verschijnt. Je zult een nieuw schijfstation zien verschijnen met de naam RPI-RP2. Sleep het bestand .uf2-bestand naar RPI-RP2.
