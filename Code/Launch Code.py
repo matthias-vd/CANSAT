d462ddf0b61fae6dd8e202a95bbed1708e529377
Matthias Van Duysen <mvd@somnis.com>
2024-03-11 09:42:31 +0100

    Bug fixes

--- Code/Launch Code.py
+++ Code/Launch Code.py
@@ -9,6 +9,8 @@ import AS7265X_sparkfun
 from AS7265X_sparkfun import AS7265X
 import AS7265X_sparkfun
 import adafruit_bmp280
+import adafruit_rfm69
+import digitalio
 
 # - Variables -
 FREQ = 433.1 
@@ -23,31 +25,38 @@ RADIO = 1
 AS7265x_LED_WHITE =	0x00 #White LED is connected to x51
 AS7265x_LED_IR =	0x01 #IR LED is connected to x52
 AS7265x_LED_UV =	0x02 #UV LED is connected to x53
-i2c = busio.I2C(board.GP17,board.GP16)
+i2c= busio.I2C(board.GP17,board.GP16)
 start = time.monotonic_ns()
 my_as7265x = AS7265X(i2c)
 my_as7265x.set_measurement_mode(AS7265X_sparkfun.MEASUREMENT_MODE_6CHAN_ONE_SHOT)
 # Test Spectroscopy
-my_as7265x.take_measurements()
-while (not my_as7265x.data_available()):
-        print("Geen data van sensor")
-        while(1)        
-        
+#my_as7265x.take_measurements()
+#while (not my_as7265x.data_available()):
+#        print("Geen data van sensor")    
+            
 # SD-card
-spi = busio.SPI(GP10, MOSI=GP11, MISO=GP12)
-cs = GP13
-sd = sdcardio.SDCard(spi, cs)
+SPI_SD = busio.SPI(GP10, MOSI=GP11, MISO=GP12)
+CS_SD = GP13
+sd = sdcardio.SDCard(SPI_SD, CS_SD)
 vfs = storage.VfsFat(sd)
 storage.mount(vfs, '/sd')
 
 # RFM69
+SPI_RFM = busio.SPI(board.GP6, board.GP7, board.GP4)
+CS_RFM = digitalio.DigitalInOut(board.GP5)
+RESET_RFM = digitalio.DigitalInOut(board.GP3)
+LED_RFM = digitalio.DigitalInOut(board.LED)
 ENCRYPTION_KEY = b"\x01\x02\x03\x04\x05\x06\x07\X08\x01\x02\x03\x04\x05\x06\x07\x08"
-rfm69 = adafruit_rfm69.RFM69(spi, CS, RESET, FREQ)
-rfm69.node = NODE_ID
+
+try: 
+    rfm69 = adafruit_rfm69.RFM69(SPI_RFM, CS_RFM, RESET_RFM, FREQ)
+    rfm69.node = NODE_ID
+except RuntimeError:
+    RADIO = 0
+    print("RFM not connected")
 
 # BMP280
-i2c = busio.I2C(board.GP9, board.GP8)
-
+#i2c_bmp280 = busio.I2C(board.GP24, board.GP25)
 bmp280 = adafruit_bmp280.Adafruit_BMP280_I2C(i2c)
 
 #Create and open file:
@@ -58,25 +67,27 @@ with open("/sd/pico.txt", "w") as file:
     my_as7265x.take_measurements_with_bulb()
     all_values_bulb = my_as7265x.get_value(1)
     if SHELL == 1:
-        print(all_values)
+        print(str(all_values))
         print()
-        print(all_values_bulb)
+        print(str(all_values_bulb))
         print()
-        print(bmp280.temperature)
+        print(str(bmp280.temperature))
         print()
-        print(bmp280.pressure)
+        print(str(bmp280.pressure))
         print()
         print()
     if RADIO == 1:
         rfm69.send(bytes("begin","utf-8"))
-        rfm69.send(bytes(all_values,"utf-8"))
+        rfm69.send(bytes(str(all_values),"utf-8"))
         rfm69.send(bytes(",","utf-8"))
-        rfm69.send(bytes(all_values_bulb,"utf-8"))
+        rfm69.send(bytes(str(all_values_bulb),"utf-8"))
         rfm69.send(bytes(",","utf-8"))
-        rfm69.send(bytes(bmp280.temperature,"utf-8"))
+        rfm69.send(bytes(str(bmp280.temperature),"utf-8"))
         rfm69.send(bytes(",","utf-8"))
-        rfm69.send(bytes(bmp280.pressure,"utf-8"))
+        rfm69.send(bytes(str(bmp280.pressure),"utf-8"))
         rfm69.send(bytes(",","utf-8"))
         rfm69.send(bytes("end","utf-8"))
+        
+        ack = rfm69.send(bytes("Message %i!" % counter , "utf-8") )
         
     sleep(SLEEP)
